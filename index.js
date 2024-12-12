834d49a4f7cdfe16cb4e18d5986c08d1b5ec79c2
FelipeArce <arceprogramando@gmail.com>
2024-12-11 23:57:24 -0300

    feat: creation of nodejs base

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (_req, res) => {
+    const htmlResponse = `
+    <html>
+        <head>
+            <title>Express</title>
+        </head>
+        <body>
+            <h1>Express Changes</h1>
+        </body>
+    </html>
+    `
+    res.send(htmlResponse);
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
